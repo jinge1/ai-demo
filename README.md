1c13810d0fe632319dcd5657fdc03f2d9e2e847e
lixiong <346959251@qq.com>
2019-10-28 00:37:39 +0800

    第一个ai算法

--- README.md
+++ README.md
@@ -32,6 +32,8 @@
 3. 进入隔离的虚拟环境
    `pipenv shell`
 
+pipenv uninstall --all 卸载全部包并从Pipfile中移除
+
 ### vscode 切换 pipenv 到虚拟环境
 
 1. 查看 pipenv 虚拟环境路径
@@ -41,6 +43,12 @@
    在 settings.json 的最后一行添加虚拟路径"python.venvPath": "1 中路径"
 
 3. 重启 vscode
+
+## pipenv的locking过程很慢解决方法
+
+- 方法1. 使用 pipenv install --skip-lock(首选)，跳过lock过程
+
+- 可以设置国内源：Pipfile文件中[source]下面url属性，比如修改成：url = "<https://pypi.tuna.tsinghua.edu.cn/simple">
 
 ### 切换 pipenv 到虚拟环境
 
@@ -56,3 +64,9 @@
 模块
 类
 函数、变量
+
+<!-- https://www.zhangshengrong.com/p/YjNKnE6ZaW/ -->
+
+<!-- https://www.cnblogs.com/wj-1314/p/10179741.html -->
+
+<!-- https://blog.csdn.net/hubingshabi/article/details/80172608 -->
---
+++ plt-demo.py
@@ -0,0 +1,40 @@
+# %%
+import matplotlib.pyplot as plt
+import numpy as np
+
+# 线性回归
+from sklearn import linear_model
+
+# 定义训练数据
+# 定义训练数据（100个二维1-30随机离散点）
+x = np.random.randint(1,30,[100,1])
+# 根据x值创建一定范围随机离散点
+y = 2 * x + np.random.randint(1,10,[100,1])
+
+# Linear Model 线性模型
+model = linear_model.LinearRegression()
+# 训练数据
+model.fit(x, y)
+# 预测
+result = model.predict(x)
+score = model.score(x, y)
+print(score)
+
+# 标记绘图说明
+plt.title("xianxianhuigui")
+plt.xlabel("x")
+plt.ylabel("y")
+
+# 绘制测试数据离散点
+plt.scatter(x, y)
+
+# 绘制训练回归线
+plt.plot(x, result, color="red")  
+
+# 预测
+test = [[15]]
+plt.scatter(test, model.predict(test), color="yellow")
+plt.show()
+
+
+# %%
